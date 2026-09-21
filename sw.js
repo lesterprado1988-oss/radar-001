b3778847fb90c3fa2e76cefb36edaa20109aafc6
lesterprado1988-oss <lesterprado1988@gmail.com>
2026-09-21 12:00:56 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const CACHE='radar-v0.8'; self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./app.js','./manifest.json'])))); self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))); self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='radar-v0.9';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./app.js','./manifest.json'])))});
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim().then(()=>caches.keys()).then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
